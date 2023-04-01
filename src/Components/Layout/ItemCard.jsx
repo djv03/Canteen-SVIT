595c2f28931b568f69e5d21067de0042f7e08fe7
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-04-01 15:51:06 +0530

    item card compelete

--- src/Components/Layout/ItemCard.jsx
+++ src/Components/Layout/ItemCard.jsx
@@ -1,12 +1,14 @@
-import "./itemCard.css";
+import "./ItemCard.css";
 
 const ItemCard = (props) => {
   return (
     <div className="item-card">
       <img src={props.itemImage} />
       <h3>{props.itemName}</h3>
-      <h2>{props.itemPrice}</h2>
-      <p>{props.itemQuantity}</p>
+      <section className="price-details">
+        <p>Quantity : {props.itemQuantity}</p>
+        <h2>₹{props.itemPrice}</h2>
+      </section>
     </div>
   );
 };
